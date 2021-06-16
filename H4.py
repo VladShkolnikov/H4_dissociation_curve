edd83d05196a48d096e87341c0d16df0d21c14ca
Blank Laptop <blanklaptop@Blanks-MacBook-Pro.local>
2021-06-17 01:01:15 +0200

    Results for H4 molecule

--- H4.py
+++ H4.py
@@ -1,41 +1,30 @@
 import sys
-import vqe_methods_add_by_one_Harper_truncation
+import vqe_methods_add_by_one
 import numpy
 import pickle
 import numpy as np
 
-
-min_dist = float(sys.argv[1])
-max_dist = float(sys.argv[2])
-num_step = int(sys.argv[3])
-id_step = int(sys.argv[4])
-
-dist_list = list(np.linspace(min_dist, max_dist, num_step))
-dist_list = list(map(lambda x: round(x, 5), dist_list))
-
-
-dist=dist_list[id_step]
-
-print(f'The list of distances to compute is: {dist_list}')
-print(f'I am the distance: {dist}')
-
-
-PoolOfHope=['XYYZIIZIZY', 'XYYYIZZZII', 'YYIZZZIZXY', 'XXZXZIIIYI', 'XYZYIZZIYI', 'XXXZIIZZZY', 'XXIIYXZZII', 'XYXZXXXYZY', 'XXIYIIXYZY', 'IIZIZZYYXY', 'ZZXZXXIIZY', 'YZZZXYZZZY', 'YXZZIZYYII', 'IXIZXXZZYI']
-Resultat=[]
+dist = float(sys.argv[1])
+print(dist)
+if __name__ == '__main__':
     
-    
-    
-    
-geometry = "Li 0 0 0; H 0 0 {}".format(dist)
-print(geometry)
-vqe_methods_add_by_one_Harper_truncation.adapt_vqe(geometry,
-	                  adapt_thresh    = 1e-8,                        #gradient threshold
-                      adapt_maxiter   = 400,                       #maximum number of ops                   
-                      Pool            = PoolOfHope,
-                      Resultat        = Resultat,
-                      bond_legth      = dist
-                      ) 
+
+    PoolOfHope=["YIXIYIYI", "ZYXIYIZY", "YIZYXIZY", "ZZYXYYII", "XXIZIIXY", "YIZYZXYI", "XIYZYZYI", "XZIIYZII", "ZXXZZXYI","XXIIIIXY", "IYYZXIZY"]
+    Resultat=[]
         
-with open('Bond_length_dependence.LiH_dissociation_curve_pickle_min_pool_{}'.format(dist) , 'wb') as handle:
-    pickle.dump(Resultat, handle, protocol=pickle.HIGHEST_PROTOCOL)                        
+        
+    
+        
+    geometry = [('H', (0, 0, 0)), ('H', (0, 0, dist)), ('H', (0, 0, 2*dist)), ('H', (0, 0, 3*dist))]
+    print(geometry)
+    vqe_methods_add_by_one.adapt_vqe(geometry,
+    	                  adapt_thresh    = 1e-8,                        #gradient threshold
+                          adapt_maxiter   = 400,                       #maximum number of ops                   
+                          Pool            = PoolOfHope,
+                          Resultat        = Resultat,
+                          bond_legth      = dist
+                          ) 
+            
+    with open('Bond_length_dependence.LiH_dissociation_curve_pickle_min_pool_{}'.format(dist) , 'wb') as handle:
+        pickle.dump(Resultat, handle, protocol=pickle.HIGHEST_PROTOCOL)                        
 
--- vqe_methods_add_by_one.py
+++ vqe_methods_add_by_one.py
@@ -12,15 +12,33 @@ import scipy.sparse.linalg
 import operator_pools
 from tVQE import tUCCSD
 
-#from openfermion import *
-#from pyscf_backend import *
-#from of_translator import *
+import openfermion, openfermionpsi4
+from openfermion import *
+from pyscf_backend import *
+from of_translator import *
 
-QubitNumber=10
+QubitNumber=8
 
 
 
+def HartreeFock(molecule):
 
+    #exps=rotate_me()
+    
+    hamiltonian_op = molecule.get_molecular_hamiltonian()  
+    
+
+    hamiltonian_f = openfermion.transforms.get_fermion_operator(hamiltonian_op)
+    hamiltonian_jw = openfermion.transforms.jordan_wigner(hamiltonian_f)
+    hamiltonian = openfermion.transforms.get_sparse_operator(hamiltonian_jw)
+    reference_ket = scipy.sparse.csc_matrix(
+    openfermion.jw_configuration_state(
+    list(range(0,molecule.n_electrons)), molecule.n_qubits)).transpose()
+
+
+    
+    
+    return reference_ket, hamiltonian
 
 def adapt_vqe(geometry,
         adapt_conver    = 'norm',
@@ -33,69 +51,40 @@ def adapt_vqe(geometry,
 
         ):
 
-    #Basis set has to be something recognized by PySCF.  (sto-3g, 6-31g, etc.)
-    basis = "sto-3g"
-    
-    #I only trust RHF right now- I can debug open-shell refs if you need them though.
-    reference = "rhf"
-    
-    #Number of frozen electrons.  Probably an even number.
-    frozen_core = 2
-    
-    #Number of frozen virtual orbitals.
-    frozen_vir = 0
-    try:
-        file = open('MolecularHamiltonians/MolecularHamiltonian_{}'.format(bond_legth),'rb')
-        objectsFromFile = pickle.load(file)
-        
-        _0body_H = objectsFromFile[0][0]
-        _1body_H = objectsFromFile[0][1]
-        _2body_H = objectsFromFile[0][2] 
-        _1rdm = objectsFromFile[0][3] 
-        hf_energy = objectsFromFile[0][4] 
- 
-        
-        H = objectsFromFile[1][0]
-        ref = objectsFromFile[1][1]
-        N_qubits = objectsFromFile[1][2] 
-        S2 = objectsFromFile[1][3] 
-        Sz = objectsFromFile[1][4] 
-        Nop = objectsFromFile[1][5] 
-        
-        N_e = int(np.trace(_1rdm))
-    except:
-        
-        _0body_H, _1body_H, _2body_H, _1rdm, hf_energy = get_integrals(geometry, basis, reference, frozen_core = frozen_core, frozen_vir = frozen_vir)
-        N_e = int(np.trace(_1rdm))
-        #Use OpenFermion to build sparse matrix representations of JW-transformed operators:
-        H, ref, N_qubits, S2, Sz, Nop = of_from_arrays(_0body_H, _1body_H, _2body_H, N_e)
-        with open('MolecularHamiltonians/MolecularHamiltonian_{}'.format(bond_legth) , 'wb') as myHandle:
-            pickle.dump(((_0body_H, _1body_H, _2body_H, _1rdm, hf_energy),(H, ref, N_qubits, S2, Sz, Nop)), myHandle, protocol=pickle.HIGHEST_PROTOCOL) 
-    #, ref, N_qubits, S2, Sz, Nop)
-    #H is the Hamiltonian, ref is the HF reference in the active space.  S2, S_z, and N_op are the S^2, S_z, and number operators if you happen to want them.
-    
-    ci = np.linalg.eigh(H.todense())[0][0]
-    print(f"Problem reduced to {N_qubits} qubits.")
-    print(f"HF energy:                {hf_energy:20.16f}")
-    print(f"Active space FCI energy:   {ci:20.16f}")
-    print('n_electrons:',N_e)
-    print(ref.transpose().conj().dot(S2.dot(ref))[0,0].real)
+    molecule = openfermion.hamiltonians.MolecularData(geometry, "sto-3g", 1)
+    molecule = openfermionpsi4.run_psi4(molecule, 
+                run_scf = 1, 
+                run_mp2=0, 
+                run_cisd=0, 
+                run_ccsd = 0, 
+                run_fci=1, 
+                delete_input=1)
 
-
-
-    print('molecule.n_orbitals:', N_qubits//2)
-    
-    
     print(" adapt threshold:", adapt_thresh)
     print(" theta threshold:", theta_thresh)
+    print(" initial state: HF state")
 
-    pool=operator_pools.OperatorPool(Pool)  
-    reference_ket, hamiltonian=ref, H
+    print('molecule.n_atoms:',molecule.n_atoms)
+    print('molecule.n_electrons:',molecule.n_electrons)
+    print('molecule.protons:',molecule.protons)
+    print('molecule.nuclear_repulsion:',molecule.nuclear_repulsion)
+    print('molecule.n_orbitals:',molecule.n_orbitals)
+    print('molecule.n_qubitss:',molecule.n_qubits)
+    print('molecule.orbital_energies:',molecule.orbital_energies)
+    
+    #pool1=operator_pools.OperatorPool(molecule,Pool[0])
+    #pool2=operator_pools.OperatorPool(molecule,Pool[1])
+    pool=operator_pools.OperatorPool(Pool)
+
+    print(' HF energy      %20.16f au' %(molecule.hf_energy))
+    #print(' CISD energy    %20.16f au' %(molecule.cisd_energy))
+    #print(' CCSD energy    %20.16f au' %(molecule.ccsd_energy))
+    print(' FCI energy     %20.16f au' %(molecule.fci_energy))
+
+    
+    reference_ket, hamiltonian=HartreeFock(molecule)
     
 
-
-
-    HamiltonianSupport=set(['0000000011','0000001100','0000110000','0001000010','0010000001','0011000000','0100000010','0110000000','1000000001','1001000000','1100000000'])
 
 
 
@@ -109,23 +98,6 @@ def adapt_vqe(geometry,
 
     w, v = scipy.sparse.linalg.eigs(hamiltonian,k=1, which='SR')   
     GSE = min(w).real
-    a=v.transpose()[0]
-    a.real[abs(a.real) < 1e-10] = 0.0
-    a.imag[abs(a.imag) < 1e-10] = 0.0
-    #print(scipy.sparse.csr_matrix(a))
-    cx = scipy.sparse.coo_matrix(v.transpose()[0])
-    CompareSet=set()
-    for i in cx.col:
-        tmp=bin(i)[2:]
-        CompareSet.add((10-len(tmp))*'0'+tmp)
-    if CompareSet.difference(HamiltonianSupport) or HamiltonianSupport.difference(CompareSet):
-        print(CompareSet.difference(HamiltonianSupport) )
-        print( HamiltonianSupport.difference(CompareSet))
-        print(len(HamiltonianSupport))
-        print(len(CompareSet))
-        raise NameError('Unlucky error, Openfermion flipped orbitals, pool must be swaped') 
-    
-        
     print('Ground state energy:', GSE)
 
     #Thetas
@@ -206,9 +178,7 @@ def adapt_vqe(geometry,
                 flag=1
                 break
 
-    Resultat.append({'bond_length:':bond_legth, 'Pool:':Pool,'ansatz:':ansatz_ops,'parameters:':parameters,
-                     'final_error:':abs(curr_energy-GSE),'GSE:':GSE,
-                     'HF:': hf_energy,'error_before_#_step:':error})
+    Resultat.append({'bond_length:':bond_legth, 'Pool:':Pool,'ansatz:':ansatz_ops,'parameters:':parameters,'final_error:': abs(curr_energy-GSE),'GSE:':GSE})
     print('\n','\n','------------------------------------------','\n')
                         
                         
