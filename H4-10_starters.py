49f3e27122d9c865c2b24e8c964b68f2d00a8934
Vlad Shkolnikov <shkolnikov.vlad@gmail.com>
2023-03-20 23:40:44 +0100

    Fix typo 11 starters -> 10 starters
    remove checkpoints, do not print starters

--- H4-10_starters.py
+++ H4-10_starters.py
@@ -11,18 +11,18 @@ print(dist)
 if __name__ == '__main__':
     
 
-    eleven_starters_pool=["YIXIYIYI", "ZYXIYIZY", "YIZYXIZY", "ZZYXYYII", "XXIZIIXY", "YIZYZXYI", "XIYZYZYI", "XZIIYZII", "ZXXZZXYI","XXIIIIXY", "IYYZXIZY"]
+    ten_starters_pool=["YIXIYIYI", "ZYXIYIZY", "YIZYXIZY", "ZZYXYYII", "XXIZIIXY", "YIZYZXYI", "XIYZYZYI", "XZIIYZII", "ZXXZZXYI","XXIIIIXY", "IYYZXIZY"]
     Resultat=[]
     geometry = [('H', (0, 0, 0)), ('H', (0, 0, dist)), ('H', (0, 0, 2*dist)), ('H', (0, 0, 3*dist))]
     print(geometry)
     vqe_methods_add_by_one.adapt_vqe(geometry,
     	                  adapt_thresh    = 1e-8,                        #gradient threshold
                           adapt_maxiter   = 400,                       #maximum number of ops                   
-                          Pool            = eleven_starters_pool,
+                          Pool            = ten_starters_pool,
                           Resultat        = Resultat,
                           bond_legth      = dist
                           ) 
             
-    with open('Bond_length_dependence.eleven_starters_{}'.format(dist) , 'wb') as handle:
+    with open('Bond_length_dependence.ten_starters_{}'.format(dist) , 'wb') as handle:
         pickle.dump(Resultat, handle, protocol=pickle.HIGHEST_PROTOCOL)                        
 
